5beb334775026c2b3475df61d14b8da42870aad3
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-06 00:07:11 +0300

    Add project files.

---
+++ BaseGeometrik.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorfihism
+{
+    public class BaseGeometric
+    {
+        //defined the main class property
+        public double Height { get; set; }
+        public double Weight { get; set; }
+
+        // defined the main virtul method for the overriding
+        public virtual double AreaCalculate(double _height, double _weight)
+        {
+            return _height * _weight;// main calculate the area 
+        }
+
+
+        public class Triangle : BaseGeometric
+        {
+            public override double AreaCalculate(double _height, double _weight)
+            {
+
+                return (_height * _weight) / 2;// if it it geometric shape is triangle we ovveride the main virtual method
+            }
+        }
+
+
+        public class Square : BaseGeometric
+        {
+
+            public override double AreaCalculate(double _height, double _weight)
+            {
+
+                if (_height == _weight)
+                {
+                    return _height * _height; //if it is square the paramteres must to be eqqual we added if statment control 
+
+                }
+                else 
+                {
+                    return 0; // else return 0
+                }
+
+           }
+
+        }
+            public class Rctaangle : BaseGeometric
+            {
+
+                public override double AreaCalculate(double _height, double _weight)
+                {
+                    return base.AreaCalculate(_height, _weight);//it is recatangle and it isn't has any spesfiaction so we use the base callculate
+                }
+
+            
+        }
+    }
+}
+
+
+
+
+
+
+    
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using PolyMorfihism;
+using System;
+using System.Drawing;
+using static PolyMorfihism.BaseGeometric;
+namespace Polly
+    
+    {
+
+
+    class Polly
+    {
+        public static void Main(string[] args) {
+
+
+            BaseGeometric sqr = new Square(); // we produce instance for the square class
+
+            Console.WriteLine($"Square is Area:{sqr.AreaCalculate(10, 10)} M^2"); 
+            
+            
+            BaseGeometric rct = new Rctaangle();// we produce instance for the rectangle class
+
+            Console.WriteLine($"Rectangle is Area:{rct.AreaCalculate(10, 12)} M^2");
+
+            BaseGeometric trg = new Triangle();// we produce instance for the triangle class
+
+            Console.WriteLine($"Triangle is Area:{trg.AreaCalculate(10, 12)} M^2");
+
+
+
+
+        }
+
+    }
+
+}
